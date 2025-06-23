2cee7b0dbf3e508ecbd879dddf4023691c0031cc
Seth Underwood <Seth.Underwood@noaa.gov>
2025-06-23 12:35:30 -0400

    Initial import
    Only a very basic landing page

---
+++ docs/conf.py
@@ -0,0 +1,90 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# VSCode code spell check settings
+# cSpell:enableCompoundWords
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import datetime as dt
+import os
+import packaging.version
+import sphinx_rtd_theme
+
+project = "NOAA GFDL Documentation"
+copyright = f"{dt.datetime.now().year}, NOAA GFDL"
+author = "NOAA GFDL"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+extensions = [
+    "sphinx_rtd_theme",
+]
+
+# Additional Sphinx templates
+templates_path = ["_templates"]
+
+# Options for HTML output ----------------------------------------------------
+html_title = "NOAA GFDL Documentation"
+html_logo = "_static/NOAA_logo_header.png"
+html_favicon = "_static/favicon.ico"
+html_theme = "sphinx_rtd_theme"
+html_show_copyright = False
+
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL",
+                              "https://docs.gfdl.noaa.gov/")
+
+html_static_path = [
+    "_static",
+]
+
+html_css_files = [
+    "css/theme_overrides.css",
+]
+
+html_context = {    # type: ignore
+    "display_github": True,
+    "github_user": "underwoo",
+    "github_repo": "noaa-gfdl",
+    "github_version": "main",
+    "conf_py_path": "/docs/",
+    "show_sphinx": False,
+    "project_home_url": "https://www.gfdl.noaa.gov/",
+    "project_home_name": "NOAA Geophysical Fluid Dynamics Laboratory",
+    "noaa_footer_links": {
+        "Protecting Your Privacy": "https://www.noaa.gov/protecting-your-privacy",
+        "FOIA": "https://www.noaa.gov/information-technology/foia",
+        "Information Quality": "https://www.noaa.gov/organization/information-technology/policy-oversight/information-quality",
+        "Accessibility": "https://www.noaa.gov/accessibility",
+        "Guidance": "https://www.noaa.gov/guidance",
+        "Budget & Performance": "https://www.noaa.gov/budget-finance-performance",
+        "Disclaimer": "https://www.noaa.gov/disclaimer",
+        "EEO": "https://www.noaa.gov/inclusion-and-civil-rights",
+        "No-Fear Act": "https://www.noaa.gov/organization/inclusion-and-civil-rights/no-fear-act",
+        "USA.gov": "https://www.usa.gov/",
+        "Ready.gov": "https://www.ready.gov/",
+    }
+}
+
+# RTD theme options ----------------------------------------------------------
+# https://sphinx-rtd-theme.readthedocs.io/en/stable/configuring.html
+html_theme_options = {   # type: ignore
+    "collapse_navigation": False,
+    "sticky_navigation": True,
+    "navigation_depth": 4,
+    "style_external_links": True,
+    "style_nav_header_background": "#efefef",
+    "logo_only": True,
+    "version_selector": True,
+    "language_selector": True,
+    "vcs_pageview_mode": "blob",
+    "includehidden": True,
+}
+
+if (packaging.version.Version(sphinx_rtd_theme.__version__) >=
+    packaging.version.Version("2.1.0")):
+    html_theme_options["flyout_display"] = "hidden"
+
---
+++ docs/index.rst
@@ -0,0 +1,11 @@
+#######################
+NOAA GFDL Documentation
+#######################
+
+.. _gfdl: https://www.gfdl.noaa.gov
+.. _noaa: https://www.noaa.gov
+
+Welcome to the documentation for the `NOAA`_ `Geophysical Fluid Dynamics
+Laboratory <gfdl_>`_ (GFDL) developed software.  This page contains links to
+the documentation for may of the software projects developed at GFDL.
+
